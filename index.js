b8e8d7cba5c653c9399f5f3aa8312ece0685a7fc
afan <123>
2016-06-11 14:53:08 +0800

    app.json

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.use(express.query());
 app.use('/', wechat(config).text(function (message, req, res, next) {
     request('http://sandbox.api.simsimi.com/request.p?key=8de56a1d-5367-41bc-a36a-4be3d46b5742&lc=zh&ft=1.0&text=' + encodeURIComponent(message.Content), function (err, response, data) {
         if(err || !JSON.parse(data).response){
-            res.reply({
+            return res.reply({
                 content: '机器人已经不堪重负挂掉了呢,请稍后再来调戏吧- -!',
                 type: 'text'
             });
